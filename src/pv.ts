48d0b4caa91ed9d45e74fb79f250a949c7f81840
Pavel Pipus <pavel.pipus@gmail.com>
2019-05-10 00:07:40 +0300

    SFN-003 | Implement PV function

--- src/pv.ts
+++ src/pv.ts
@@ -1,9 +1,13 @@
-export default (rate: number, nper: number, pmt: number) => {
+/**
+ * Calculates the present value of a loan or an investment, based on a constant interest rate
+ * @param {number} rate The interest rate per period
+ * @param {number} periods The total number of payment periods in an annuity
+ * @param {number} payment The payment made each period and cannot change over the life of the annuity
+ */
+export default function pv(rate: number, periods: number, payment: number) {
     if (rate === 0) {
-        return -pmt * nper;
+        return -payment * periods;
     }
 
-    const c1 = Math.pow(1 + rate, nper);
-
-    return (((1 - c1) / rate) * pmt * (1 + rate)) / c1;
-};
+    return (-payment / rate) * (1 - Math.pow(1 + rate, -periods));
+}
